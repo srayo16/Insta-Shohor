1c79e652bf28faff1356018e566237b387fa1846
srayo16 <srayosikder@gmail.com>
2022-03-11 01:43:48 +0600

    Alhamdulillah, all are added

--- js/app.js
+++ js/app.js
@@ -45,16 +45,19 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById('queAns').style.display = "block";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById('queAns').style.display = "none";
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
+        document.getElementById('queAns').style.display = "none";
 
         displayReportedPosts();
     }
